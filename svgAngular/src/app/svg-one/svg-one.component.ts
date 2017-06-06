bad11b15bcc3736d8f95cad56b28510641325df8
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-05 22:27:40 -0400

    width and height defined for the rectangle

--- svgAngular/src/app/svg-one/svg-one.component.ts
+++ svgAngular/src/app/svg-one/svg-one.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SvgOneChildComponent } from '../svg-one-child/svg-one-child.component';
 @Component({
   selector: 'svg-one',
@@ -6,12 +6,24 @@ import { SvgOneChildComponent } from '../svg-one-child/svg-one-child.component';
   styleUrls: ['./svg-one.component.css']
 })
 export class SvgOneComponent implements OnInit {
-    width : number=500;
-    height : number;
+    viewbox_width : number=200 ;
+    viewbox_height : number=200;
+    rect_width : number=50 ;
+    rect_height : number=50;
+    
+  //make accesable to svg element
+  @ViewChild("svgelement") svgElm : any;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+/** 
+     * Get viewbox based on element width / height
+     */
+    public getViewbox() : string {           
+        return "0 0 "+
+            this.viewbox_width +" "+
+            this.viewbox_height;
+    }
 }
