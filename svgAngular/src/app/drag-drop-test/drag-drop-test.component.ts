e218beb42f0e696e77211fbe86443318b9b37880
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-06 02:19:55 -0400

    coment added in drag drop test component

--- svgAngular/src/app/drag-drop-test/drag-drop-test.component.ts
+++ svgAngular/src/app/drag-drop-test/drag-drop-test.component.ts
@@ -13,19 +13,34 @@ dropZone='Drop Zone';
   }
   drag_handler(ev) {
     console.log("Drag");
-    //console.log(ev);
+    // console.log(ev);
   }
 
   dragstart_handler(ev) {
     console.log("dragStart");
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
+    // Identify what is draggable
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+    
+    // Define the drag's data
+    ev.dataTransfer.setData("text/html", "<p>Example paragraph</p>");
+    ev.dataTransfer.setData("text/uri-list", "http://developer.mozilla.org");
+  
+    // Define the drag image
+    var img = new Image(); 
+    img.src = './src/app/drag-drop-test/dkitaw.png'; 
+    ev.dataTransfer.setDragImage(img, 10, 10);
+
+    // Define the drag effect
+    ev.dataTransfer.dropEffect="copy";
+}
 
   drop_handler(ev) {
     console.log("Drop******");
+    console.log(ev.dataTransfer);
     ev.currentTarget.style.background = "lightyellow";
 
     ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
      ev.target.appendChild(document.getElementById(data));
@@ -34,7 +49,11 @@ dropZone='Drop Zone';
   dragover_handler(ev) {
     console.log("dragOver");
     this.dropZone="Drop it here";
+    // Define a drop zone
     ev.preventDefault();
+    // Set the dropEffect to move
+    ev.dataTransfer.dropEffect = "move"
+
   }
   dragleave_handler(ev) {
     console.log("dragleave");
